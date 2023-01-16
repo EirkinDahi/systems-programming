c8ae61da821b68b1733e34bea446cf7f4542276c
An <an.phxm@gmail.com>
2023-01-16 14:50:05 -0500

    added sum file

---
+++ a01/sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    int num1, num2, sum;
+
+    printf("Input the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Input the second integer: ");
+    scanf("%d", &num2);
+
+    //calculations 
+    sum = num1 + num2;
+
+    printf("Sum of the above two integers = %d", sum);
+
+    return 0;
+}
