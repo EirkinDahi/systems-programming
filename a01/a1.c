b65fa37b6004ef67ead082a693ade4f6aa610a68
Eirkin Dahi <eirkin12@gmail.com>
2023-01-28 23:20:49 -0500

    add statement for error

--- a01/a1.c
+++ a01/a1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 int main(int argc, char *argv[]){
+    if(argc == 1){
+        printf("No arguments given...Exiting program\n");
+        exit(1);
+    }
     for(int i = argc - 1; i > 0; i--){
         printf("%s ", argv[i]);
     }
