9a9facb93b0ed1aa8e59a4cb0fcb9571281f1754
Eirkin Dahi <eirkin12@gmail.com>
2023-02-08 13:30:54 -0500

    changes

--- a02/sort.c
+++ a02/sort.c
@@ -6,29 +6,29 @@ void selectionSort(int arr[],int n);
 int partition(int arr[], int low, int high);
 void quickSort(int arr[], int low, int high);
 
-const int MAX = 10;
+const int SIZE = 10;
 
 int main(int argc, char *argv[]){
 
     int i= 0;
-    int a[MAX];
-    int b[MAX];
+    int a[SIZE];
+    int b[SIZE];
     // int *ptr = &a;
     // int *ptr_b = &b;
 
     // printf("%d ", *(ptr + 1));
-    for(i = 0; i < MAX; i++){
-        *(a + i) = rand() % MAX + 1;
+    for(i = 0; i < SIZE; i++){
+        *(a + i) = rand() % SIZE + 1;
         printf("%d \n", *(a + i));
     }
     printf("\n");
-    for(i = 0; i < MAX; i++){
+    for(i = 0; i < SIZE; i++){
         *(b + i) = *(a+ i); 
         printf("%d \n", *(b+ i));
     }
     printf("\n");
-    quickSort(b, 0, MAX -1);
-    for(i = 0; i < MAX; i++){
+    quickSort(b, 0, SIZE -1);
+    for(i = 0; i < SIZE; i++){
         printf("%d \n", *(b+ i));
     }
 
